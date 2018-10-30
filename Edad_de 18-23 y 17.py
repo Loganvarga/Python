d38b149e3d7a44c5cb0e0243fc3737178f323339
logan varga <44461458+Loganvarga@users.noreply.github.com>
2018-10-30 12:05:50 +0100

    Create Edad_de 18-23 y 17.py

---
+++ Edad_de 18-23 y 17.py
@@ -0,0 +1,17 @@
+  Edad= 0;
+  permiso=("Puedes pasar")
+  edad=int(input("introduce su edad"))
+
+  if edad =18
+     permiso=("Puedes pasar")
+
+  if edad =23
+     permiso=("Puedes pasar")
+
+  if edad =17
+     permiso=("Puedes pasar")
+
+  if edad =18 and edad >23 and edad <17
+            permiso=("No puedes pasar")
+
+  Print("tienes";edad;permisos)
